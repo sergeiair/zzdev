ec4d08593143cba8d621ad810d4a68334305d305
Siarhei Zhardzetski <zsggem@gmail.com>
2023-01-30 16:48:02 +0100

    ktor works

--- androidApp/src/main/java/com/zzdev/app/android/MainActivity.kt
+++ androidApp/src/main/java/com/zzdev/app/android/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.zzdev.app.Greeting
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +20,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    val scope = rememberCoroutineScope()
+                    var text by remember { mutableStateOf("Loading") }
+
+                    LaunchedEffect(true) {
+                        scope.launch {
+                            text = try {
+                                Greeting().greeting()
+                            } catch (e: Exception) {
+                                e.localizedMessage ?: "error"
+                            }
+                        }
+                    }
+
+                    GreetingView(text)
                 }
             }
         }
--- shared/src/commonMain/kotlin/com/zzdev/app/Greeting.kt
+++ shared/src/commonMain/kotlin/com/zzdev/app/Greeting.kt
@@ -1,9 +1,20 @@
 package com.zzdev.app
 
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
+    private val client = HttpClient()
+
     fun greet(): String {
         return "Hello, ${platform.name}!"
+    }
+
+    suspend fun greeting(): String {
+        val response = client.get("https://ktor.io/docs/")
+        return response.bodyAsText()
     }
 }
